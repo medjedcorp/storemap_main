949ca1221495d31a908fb4baa7d56bbd8fdcc9ca
Tomoya Matsuda <matsuda@storemap.jp>
2021-06-24 15:28:50 +0900

    細かな修正

--- app/Jobs/StoreImportCsvJob.php
+++ app/Jobs/StoreImportCsvJob.php
@@ -158,36 +158,17 @@ class StoreImportCsvJob implements ShouldQueue
       $parking = 'parking';
 
       foreach ($csv as $row_data => $v) {
-
+        
         $geo = Store::where('company_id', $this->user->company_id)->where('store_code', $v['store_code'])->first();
-
-        // 配列の中に値があるかチェック、あればtrue
-        $kana_bool = in_array($store_kana, $v);
-        $fax_bool = in_array($store_fax_number, $v);
-        $email_bool = in_array($store_email, $v);
-        $img1_bool = in_array($store_img1, $v);
-        $img2_bool = in_array($store_img2, $v);
-        $img3_bool = in_array($store_img3, $v);
-        $img4_bool = in_array($store_img4, $v);
-        $img5_bool = in_array($store_img5, $v);
-        $info_bool = in_array($store_info, $v);
-        $store_bool = in_array($store_url, $v);
-        $flyer_bool = in_array($flyer_img, $v);
-        $floor_bool = in_array($floor_guide, $v);
-        $pay_info = in_array($pay_info, $v);
-        $access = in_array($access, $v);
-        $opening_hour = in_array($opening_hour, $v);
-        $closed_day = in_array($closed_day, $v);
-        $parking = in_array($parking, $v);
 
         // 登録可能件数を超えた場合のエラー処理
         // 店舗数を毎回カウント
-        // $limit_code = Store::where('company_id', $this->user->company_id)->whereIn('store_code', $check_code)->count();
         // 現在の店舗数をカウント
         $now_code = Store::where('company_id', $this->user->company_id)->count();
         // 上限を超えた場合はエラー処理
         // if($quantity <= $limit_code or $quantity <= $now_code){
-        if($quantity <= $now_code){
+          // dd($geo);
+        if($quantity < $now_code){
           $fname = $cid . '/csv/error/' . 'stores_' . date('YmdHis') . '.txt';
           Storage::disk('public')->put($fname, "");
           $path = url('storage/' . $fname);
@@ -201,73 +182,75 @@ class StoreImportCsvJob implements ShouldQueue
           CsvFileDeleteJob::dispatch($path)->delay(now()->addDays(3));
           break;
         }
-
+        
         if (isset($geo)) {
           // $geoに値があるとき
+          
           if ($geo->prefecture == $v['prefecture'] || $geo->store_city == $v['store_city'] || $geo->store_adnum == $v['store_adnum'] || $geo->store_apart == $v['store_apart']) {
             // 住所を比較して同じだった場合はジオコーディングせずに保存
             $geo->store_name = $v['store_name'];
-            if($kana_bool){
+            if(isset($v['store_kana'])){
               $geo->store_kana = $v['store_kana'];
             }
             $geo->store_postcode = $v['store_postcode'];
             $geo->store_phone_number = $v['store_phone_number'];
-            if($fax_bool){
+            if(isset($v['store_fax_number'])){
               $geo->store_fax_number = $v['store_fax_number'];
             }
-            if($email_bool){
+            if(isset($v['store_email'])){
               $geo->store_email = $v['store_email'];
             }
             $geo->pause_flag = $v['pause_flag'];
-            if($img1_bool){
+            if(isset($v['store_img1'])){
               $geo->store_img1 = $v['store_img1'];
             }
-            if($img2_bool){
+            if(isset($v['store_img2'])){
               $geo->store_img2 = $v['store_img2'];
             }
-            if($img3_bool){
+            if(isset($v['store_img3'])){
               $geo->store_img3 = $v['store_img3'];
             }
-            if($img4_bool){
+            if(isset($v['store_img4'])){
               $geo->store_img4 = $v['store_img4'];
             }
-            if($img5_bool){
+            if(isset($v['store_img5'])){
               $geo->store_img5 = $v['store_img5'];
             }
-            if($info_bool){
+            if(isset($v['store_info'])){
               $geo->store_info = $v['store_info'];
             }
             $geo->industry_id = $v['industry_id'];
-            if($store_bool){
+            if(isset($v['store_url'])){
               $geo->store_url = $v['store_url'];
             }
-            if($flyer_bool){
+            if(isset($v['flyer_img'])){
               $geo->flyer_img = $v['flyer_img'];
             }
-            if($floor_bool){
+            if(isset($v['floor_guide'])){
               $geo->floor_guide = $v['floor_guide'];
             }
-            if($pay_info){
+            if(isset($v['pay_info'])){
               $geo->pay_info = $v['pay_info'];
             }
-            if($access){
-              $geo->floor_guide = $v['access'];
+            if(isset($v['access'])){
+              $geo->access = $v['access'];
             }
-            if($opening_hour){
-              $geo->floor_guide = $v['opening_hour'];
+            if(isset($v['opening_hour'])){
+              $geo->opening_hour = $v['opening_hour'];
             }
-            if($closed_day){
-              $geo->floor_guide = $v['closed_day'];
+            if(isset($v['closed_day'])){
+              $geo->closed_day = $v['closed_day'];
             }
-            if($parking){
-              $geo->floor_guide = $v['parking'];
+            if(isset($v['parking'])){
+              $geo->parking = $v['parking'];
             }
             $geo->save();
-
+            // dd($geo);
             // store_codeを配列に保存
             // $check_code[] = $geo->store_code;
 
           } else {
+            
             // 住所違う場合はジオコーディングして保存
             $address = $v['prefecture'] . $v['store_city'] . $v['store_adnum'] . $v['store_apart'];
             $myKey = config('const.geo_key');
@@ -281,7 +264,7 @@ class StoreImportCsvJob implements ShouldQueue
             $lng = $jsonData["results"][0]["geometry"]["location"]["lng"];
 
             $geo->store_name = $v['store_name'];
-            if($kana_bool){
+            if(isset($v['store_kana'])){
               $geo->store_kana = $v['store_kana'];
             }
             $geo->store_postcode = $v['store_postcode'];
@@ -290,55 +273,55 @@ class StoreImportCsvJob implements ShouldQueue
             $geo->store_adnum = $v['store_adnum'];
             $geo->store_apart = $v['store_apart'];
             $geo->store_phone_number = $v['store_phone_number'];
-            if($fax_bool){
+            if(isset($v['store_fax_number'])){
               $geo->store_fax_number = $v['store_fax_number'];
             }
-            if($email_bool){
+            if(isset($v['store_email'])){
               $geo->store_email = $v['store_email'];
             }
             $geo->pause_flag = $v['pause_flag'];
-            if($img1_bool){
+            if(isset($v['store_img1'])){
               $geo->store_img1 = $v['store_img1'];
             }
-            if($img2_bool){
+            if(isset($v['store_img2'])){
               $geo->store_img2 = $v['store_img2'];
             }
-            if($img3_bool){
+            if(isset($v['store_img3'])){
               $geo->store_img3 = $v['store_img3'];
             }
-            if($img4_bool){
+            if(isset($v['store_img4'])){
               $geo->store_img4 = $v['store_img4'];
             }
-            if($img5_bool){
+            if(isset($v['store_img5'])){
               $geo->store_img5 = $v['store_img5'];
             }
-            if($info_bool){
+            if(isset($v['store_info'])){
               $geo->store_info = $v['store_info'];
             }
             $geo->industry_id = $v['industry_id'];
-            if($store_bool){
+            if(isset($v['store_url'])){
               $geo->store_url = $v['store_url'];
             }
-            if($flyer_bool){
+            if(isset($v['flyer_img'])){
               $geo->flyer_img = $v['flyer_img'];
             }
-            if($floor_bool){
+            if(isset($v['floor_guide'])){
               $geo->floor_guide = $v['floor_guide'];
             }
-            if($pay_info){
+            if(isset($v['pay_info'])){
               $geo->pay_info = $v['pay_info'];
             }
-            if($access){
+            if(isset($v['access'])){
               $geo->access = $v['access'];
             }
-            if($opening_hour){
+            if(isset($v['opening_hour'])){
               $geo->opening_hour = $v['opening_hour'];
             }
-            if($closed_day){
+            if(isset($v['closed_day'])){
               $geo->closed_day = $v['closed_day'];
             }
-            if($parking){
-              $geo->floor_guide = $v['parking'];
+            if(isset($v['parking'])){
+              $geo->parking = $v['parking'];
             }
             $geo->location = DB::raw("ST_GeomFromText('POINT(" . $lat . " " . $lng . ")')");
             // $geo->latitude = $latitude;
@@ -349,13 +332,13 @@ class StoreImportCsvJob implements ShouldQueue
             // $check_code[] = $geo->store_code;
           }
         } else {
+          dd($v);
           // geo に値がないときジオコーディングして新規保存
           if($v['store_apart']){
             $address = $v['prefecture'] . $v['store_city'] . $v['store_adnum'] . $v['store_apart'];
           } else {
             $address = $v['prefecture'] . $v['store_city'] . $v['store_adnum'];
           }
-          
           
           // $myKey = env('GOOGLE_MAPS_API_KEY');
           $myKey = config('const.geo_key');
@@ -376,11 +359,8 @@ class StoreImportCsvJob implements ShouldQueue
           $store->company_id = $this->user->company_id;
           $store->store_code = $v['store_code'];
           $store->store_name = $v['store_name'];
-          // if($store_kana){
-          //   $store->store_kana = $store_kana;
-          // }
-          if($kana_bool){
-            $store->store_kana = $v['store_kana'];
+          if(isset($v['store_kana'])){
+            $geo->store_kana = $v['store_kana'];
           }
           $store->store_postcode = $v['store_postcode'];
           $store->prefecture = $v['prefecture'];
@@ -388,56 +368,55 @@ class StoreImportCsvJob implements ShouldQueue
           $store->store_adnum = $v['store_adnum'];
           $store->store_apart = $v['store_apart'];
           $store->store_phone_number = $v['store_phone_number'];
-          if($fax_bool){
-            $store->store_fax_number = $v['store_fax_number'];
+          if(isset($v['store_fax_number'])){
+            $geo->store_fax_number = $v['store_fax_number'];
           }
-          if($email_bool){
-            $store->store_email = $v['store_email'];
+          if(isset($v['store_email'])){
+            $geo->store_email = $v['store_email'];
           }
           $store->pause_flag = $v['pause_flag'];
-
-          if($img1_bool){
-            $store->store_img1 = $v['store_img1'];
+          if(isset($v['store_img1'])){
+            $geo->store_img1 = $v['store_img1'];
           }
-          if($img2_bool){
-            $store->store_img2 = $v['store_img2'];
+          if(isset($v['store_img2'])){
+            $geo->store_img2 = $v['store_img2'];
           }
-          if($img3_bool){
-            $store->store_img3 = $v['store_img3'];
+          if(isset($v['store_img3'])){
+            $geo->store_img3 = $v['store_img3'];
           }
-          if($img4_bool){
-            $store->store_img4 = $v['store_img4'];
+          if(isset($v['store_img4'])){
+            $geo->store_img4 = $v['store_img4'];
           }
-          if($img5_bool){
-            $store->store_img5 = $v['store_img5'];
+          if(isset($v['store_img5'])){
+            $geo->store_img5 = $v['store_img5'];
           }
-          if($info_bool){
-            $store->store_info = $v['store_info'];
+          if(isset($v['store_info'])){
+            $geo->store_info = $v['store_info'];
           }
           $store->industry_id = $v['industry_id'];
-          if($store_bool){
-            $store->store_url = $v['store_url'];
+          if(isset($v['store_url'])){
+            $geo->store_url = $v['store_url'];
           }
-          if($flyer_bool){
-            $store->flyer_img = $v['flyer_img'];
+          if(isset($v['flyer_img'])){
+            $geo->flyer_img = $v['flyer_img'];
           }
-          if($floor_bool){
-            $store->floor_guide = $v['floor_guide'];
+          if(isset($v['floor_guide'])){
+            $geo->floor_guide = $v['floor_guide'];
           }
-          if($pay_info){
-            $store->pay_info = $v['pay_info'];
+          if(isset($v['pay_info'])){
+            $geo->pay_info = $v['pay_info'];
           }
-          if($access){
-            $store->access = $v['access'];
+          if(isset($v['access'])){
+            $geo->access = $v['access'];
           }
-          if($opening_hour){
-            $store->opening_hour = $v['opening_hour'];
+          if(isset($v['opening_hour'])){
+            $geo->opening_hour = $v['opening_hour'];
           }
-          if($closed_day){
-            $store->closed_day = $v['closed_day'];
+          if(isset($v['closed_day'])){
+            $geo->closed_day = $v['closed_day'];
           }
-          if($parking){
-            $store->floor_guide = $v['parking'];
+          if(isset($v['parking'])){
+            $geo->parking = $v['parking'];
           }
           // $store->latitude = $latitude;
           // $store->longitude = $longitude;
