7a97d6fb1bc1658dc6520a77fc563a59f3eb8345
Tomoya Matsuda <matsuda@storemap.jp>
2021-06-28 23:48:01 +0900

    位置情報修正

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -189,7 +189,7 @@ class ResultController extends Controller
         ->selectRaw("id,store_name,store_img1,store_postcode,prefecture,store_city,store_adnum,store_apart,store_phone_number,store_email,store_info,ST_X( location ) As latitude, ST_Y( location ) As longitude, ROUND(ST_LENGTH(ST_GEOMFROMTEXT( CONCAT('LineString( " . $lat . " " . $lng . " , ' , ST_X( location ),' ', ST_Y( location ),')'))) * 112.12 * 1000 ) AS distance") // 距離を計測。distanceに距離を代入
         ->orderByRaw('distance IS NULL ASC') // Nullは最後尾に
         ->orderBy('distance', 'ASC') //遠い順、近い順
-        ->limit(200)
+        ->limit(10)
         ->toSql();
 
       // お店情報を取得
