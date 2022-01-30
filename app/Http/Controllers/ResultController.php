74f391178e480aa447f2f6730b77555b2c5da8a1
Tomoya Matsuda <matsuda@storemap.jp>
2022-01-30 17:47:03 +0900

    storeモデル変更

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -167,15 +167,15 @@ class ResultController extends Controller
         $req_city = $first_place->city;
         // 市だけの場合
         $store_data = Store::with('company')
-        ->with('')
-        ->where('prefecture', $first_place->region)
-        ->where('store_city', 'like', '%' . $first_place->city . '%')
-        ->ActiveStore()
-        ->selectRaw("id,store_name,store_img1,store_postcode,prefecture,store_city,store_adnum,store_apart,store_phone_number,store_email,store_info,ST_X( location ) As latitude, ST_Y( location ) As longitude, ROUND(ST_LENGTH(ST_GEOMFROMTEXT( CONCAT('LineString( " . $lat . " " . $lng . " , ' , ST_X( location ),' ', ST_Y( location ),')'))) * 112.12 * 1000 ) AS distance") // 距離を計測。distanceに距離を代入
-        ->orderByRaw('distance IS NULL ASC') // Nullは最後尾に
-        ->orderBy('distance', 'ASC') //遠い順、近い順
-        ->limit(200)
-        ->get();
+          ->with('')
+          ->where('prefecture', $first_place->region)
+          ->where('store_city', 'like', '%' . $first_place->city . '%')
+          ->ActiveStore()
+          ->selectRaw("id,store_name,store_img1,store_postcode,prefecture,store_city,store_adnum,store_apart,store_phone_number,store_email,store_info,ST_X( location ) As latitude, ST_Y( location ) As longitude, ROUND(ST_LENGTH(ST_GEOMFROMTEXT( CONCAT('LineString( " . $lat . " " . $lng . " , ' , ST_X( location ),' ', ST_Y( location ),')'))) * 112.12 * 1000 ) AS distance") // 距離を計測。distanceに距離を代入
+          ->orderByRaw('distance IS NULL ASC') // Nullは最後尾に
+          ->orderBy('distance', 'ASC') //遠い順、近い順
+          ->limit(200)
+          ->get();
 
         // $store_data = Store::where('prefecture', $first_place->region)
         //   ->where('store_city', 'like', '%' . $first_place->city . '%')
